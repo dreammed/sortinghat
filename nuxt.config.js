0a2ddfef0f1dbaf78013331904dffd365d0d5a2e
dreammed <dreammed@gmail.com>
2022-05-11 23:10:59 +0700

    minor-fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   ],
 
   // Target: https://go.nuxtjs.dev/config-target
-  // target: 'static',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -31,12 +31,11 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/style.css',
+    // '@/assets/style.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/bootstrap-vue',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -44,15 +43,14 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    // '@nuxtjs/eslint-module',
+
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
-    // 'vue-sweetalert2/nuxt',
+
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
@@ -62,21 +60,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    babel: {
-      "presets": [
-        [
-          "@babel/preset-env",
-          {
-            targets: {
-              esmodules: true
-            },
-            "loose": true,
-            "shippedProposals": true
-          }
-        ]
-      ]
-    },
-    devtools:false,
   },
 
   server: {
